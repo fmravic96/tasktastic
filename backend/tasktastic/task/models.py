afd2f4b82db2c5717ced84da4c160d0573da7e30
Filip Mravic <filip.mravic@gmail.com>
2025-11-09 12:39:40 +0100

    Initial commit
    - add Django & Django ninja
    - add basic Task and User models
    - initial config setup

---
+++ backend/tasktastic/task/models.py
@@ -0,0 +1,17 @@
+import uuid
+from django.db import models
+from django.conf import settings
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    category = models.CharField(max_length=100)
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
